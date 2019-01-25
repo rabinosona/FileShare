4aaaccd16008453e61f7c1f108d075fbc4d547b6
rabinosona <rabinosona@gmail.com>
2019-01-25 18:31:26 +0300

    added blurred background, working on the important control elements placement

--- FileSharing/spa/fileshare/src/modules/MainModule/components/SignInComponent.jsx
+++ FileSharing/spa/fileshare/src/modules/MainModule/components/SignInComponent.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap'
-import classNames from 'classnames';
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import classNames from "classnames";
 
-import alignment from './css/SignInComponent.module.css';
+import alignment from "./css/SignInComponent.module.css";
 
 export default class SignInComponent extends Component {
   constructor(props) {
@@ -12,17 +12,53 @@ export default class SignInComponent extends Component {
 
   render() {
     return (
-      <div>
-        <div className={alignment.header}>
-          <div className={alignment.header_logo_container}>
-            <div className={alignment.header_logo}> Oche bien filehsare!</div>
+      <html>
+        <body>
+          <div className={alignment.header}>
+            <div className={alignment.header_logo_container}>
+              <div className={alignment.header_logo}> Oche bien filehsare!</div>
+            </div>
+            <Button
+              className={classNames({
+                [alignment.header_home]: true,
+                [alignment.button]: true
+              })}
+              bsStyle="link"
+            >
+              Home
+            </Button>
+            <Button
+              className={classNames({
+                [alignment.header_sign_in]: true,
+                [alignment.button]: true
+              })}
+              bsStyle="link"
+            >
+              Sign in
+            </Button>
+            <Button
+              className={classNames({
+                [alignment.header_sign_up]: true,
+                [alignment.button]: true
+              })}
+              bsStyle="link"
+            >
+              Sign up
+            </Button>
+            <FormControl
+              className={alignment.header_search}
+              bsStyle=""
+              placeholder="Enter text"
+            />
           </div>
-          <Button className={classNames({[alignment.header_home]: true, [alignment.button]: true})} bsStyle="link">Home</Button>
-          <Button className={classNames({[alignment.header_sign_in]: true, [alignment.button]: true})} bsStyle="link">Sign in</Button>
-          <Button className={classNames({[alignment.header_sign_up]: true, [alignment.button]: true})} bsStyle="link">Sign up</Button>
-          <FormControl className={alignment.header_search} bsStyle="" placeholder="Enter text" />
-        </div>
-      </div>
+          <div className={alignment.background}>
+          </div>
+          <div className={alignment.content}>
+              <div className={alignment.test}>
+              esse???</div>
+          </div>
+        </body>
+      </html>
     );
   }
 }
