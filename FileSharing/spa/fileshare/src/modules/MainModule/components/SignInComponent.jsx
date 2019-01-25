075f6e67080c047f176dab652f9e41bcde2232e8
rabinosona <rabinosona@gmail.com>
2019-01-25 17:33:01 +0300

    Main page css/html improvements

--- FileSharing/spa/fileshare/src/modules/MainModule/components/SignInComponent.jsx
+++ FileSharing/spa/fileshare/src/modules/MainModule/components/SignInComponent.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap'
+import classNames from 'classnames';
 
 import alignment from './css/SignInComponent.module.css';
 
@@ -12,8 +14,13 @@ export default class SignInComponent extends Component {
     return (
       <div>
         <div className={alignment.header}>
-          <p className={alignment.header_logo}>Oche bien filehsare!</p>
-          <Button className={alignment.header_home} bsStyle="primary">Home</Button>
+          <div className={alignment.header_logo_container}>
+            <div className={alignment.header_logo}> Oche bien filehsare!</div>
+          </div>
+          <Button className={classNames({[alignment.header_home]: true, [alignment.button]: true})} bsStyle="link">Home</Button>
+          <Button className={classNames({[alignment.header_sign_in]: true, [alignment.button]: true})} bsStyle="link">Sign in</Button>
+          <Button className={classNames({[alignment.header_sign_up]: true, [alignment.button]: true})} bsStyle="link">Sign up</Button>
+          <FormControl className={alignment.header_search} bsStyle="" placeholder="Enter text" />
         </div>
       </div>
     );
